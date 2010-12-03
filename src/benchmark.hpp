1591423850ff6863a84de26660cac95230fc7368
Carmine Paolino <earcar@gmail.com>
2010-12-03 12:50:15 +0100

    Microsecond precision for benchmarks

--- src/benchmark.hpp
+++ src/benchmark.hpp
@@ -1,10 +1,11 @@
-#include <boost/progress.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include <vector>
 #include <ostream>
 #define BENCHMARK(exp) { benchmark_timer(#exp,__FILE__,__LINE__); (exp); }
 
 struct timed_event {
-    std::string exp, file, time;
+    std::string exp, file;
+    boost::posix_time::time_duration time;
     unsigned int line;
 };
 
@@ -14,7 +15,7 @@ namespace benchmark
     inline void write_log(std::ostream &out)
     {
         for (std::vector<timed_event>::iterator i = events.begin(); i != events.end(); ++i) {
-            out << i->file << ":" << i->line << ": " << i->exp << " took " << i->time;
+            out << i->file << ":" << i->line << ": " << i->exp << " took " << boost::posix_time::to_simple_string(i->time) << std::endl;
         }
     }
 }
@@ -23,22 +24,24 @@ class benchmark_timer
 {
 public:
     benchmark_timer(const std::string exp, const std::string file, const unsigned int line) : _exp(exp), _file(file), _line(line) {
-        t = new progress_timer(oss);
+        start = new boost::posix_time::ptime(boost::posix_time::microsec_clock::universal_time());
     }
 
     virtual ~benchmark_timer() {
-        delete t;
+        end = new boost::posix_time::ptime(boost::posix_time::microsec_clock::universal_time());
         timed_event event;
         event.exp = _exp;
         event.file = _file;
         event.line = _line;
-        event.time = oss.str();
+        event.time = *end - *start;
         benchmark::events.push_back(event);
+        delete start;
+        delete end;
     }
 
 private:
     const std::string _exp, _file;
     const unsigned int _line;
-    progress_timer *t;
+    boost::posix_time::ptime *start, *end;
     std::ostringstream oss;
 };
