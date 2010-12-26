8fb869f5b1457de1be11e073b9950bc4ad6255b5
Carmine Paolino <earcar@gmail.com>
2010-12-26 17:24:36 +0100

    plot_speedup.rb: option: use always the same server

--- tests/utils/plot_speedup.rb
+++ tests/utils/plot_speedup.rb
@@ -5,7 +5,7 @@ require 'optparse'
 require './mpi_config'
 
 logger = Logger.new STDERR
-options = { :logger => logger, :user => ENV['USER'], :procs => 5 }
+options = { :logger => logger, :user => ENV['USER'], :procs => 5, :cmdopts => "--log-level=nothing", :same => false }
 
 opts = OptionParser.new do |opts|
   opts.banner = "Usage: #{$0} [options]"
@@ -15,11 +15,14 @@ opts = OptionParser.new do |opts|
   opts.on('-c', '--command STRING', "REQUIRED: The command to run") do |command|
     options[:command] = command
   end
-  opts.on('-C', '--command-options "STRING"', "Pass this quoted string to command") do |cmdopts|
+  opts.on('-C', '--command-options "STRING"', "Pass the quoted string to the command") do |cmdopts|
     options[:cmdopts] = cmdopts
   end
   opts.on('-p', '--procs NUMBER', Integer, "Max number of processes (default = 5)") do |num|
     options[:procs] = num
+  end
+  opts.on('-S', '--same-servers', "Use always the same servers") do |same|
+    options[:same] = true
   end
   opts.on_tail("-h", "--help", "Show this message") do
     puts opts
@@ -35,8 +38,14 @@ projhome = "."
 datafile = File.new "#{File.basename options[:command]}.dat", 'w'
 MPIEXEC_HOSTS_FLAG = "-H"
 
+if options[:same]
+  idlest_servers = %x{#{projhome}/idlest_servers.rb -s #{options[:servers].join(',')} -c #{options[:procs]} -u #{options[:user]}}
+end
+
 (1..options[:procs]).each do |nprocs|
-  idlest_servers = %x{#{projhome}/idlest_servers.rb -s #{options[:servers].join(',')} -c #{nprocs} -u #{options[:user]}}
+  unless options[:same]
+    idlest_servers = %x{#{projhome}/idlest_servers.rb -s #{options[:servers].join(',')} -c #{nprocs} -u #{options[:user]}}
+  end
   realcommand = "#{MPIEXEC} #{MPIEXEC_NUMPROC_FLAG} #{nprocs} #{MPIEXEC_PREFLAGS} #{MPIEXEC_HOSTS_FLAG} #{idlest_servers.chomp} #{options[:command]} #{MPIEXEC_POSTFLAGS} #{options[:cmdopts]}".gsub(/ +/, ' ')
   logger.info "Running #{realcommand}..."
   result = %x{#{realcommand}}
