666dbeb129ccea26e34a90606cc28c3bc0d60832
Carmine Paolino <earcar@gmail.com>
2011-01-28 19:12:11 +0100

    Small World graph

--- tests/performance/page_rank.cpp
+++ tests/performance/page_rank.cpp
@@ -2,6 +2,8 @@
 #include <boost/graph/metis.hpp>
 #include "graph_loaders.hpp"
 #include <boost/graph/distributed/page_rank.hpp>
+#include <boost/graph/small_world_generator.hpp>
+#include <boost/random/linear_congruential.hpp>
 
 namespace Csv
 {
@@ -56,6 +58,54 @@ namespace Csv
     };
 }
 
+namespace SmallWorld
+{
+    struct Node {
+        float pagerank;
+        template<typename Archiver>
+        void serialize(Archiver &ar, const unsigned int) {
+            ar & pagerank;
+        }
+    };
+
+    template <typename Graph>
+    class Fixture
+    {
+        typedef small_world_iterator<minstd_rand, Graph> SWGen;
+        typedef typename property_map<Graph, float Node::*>::type VertexFloatMap;
+        typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
+
+        Graph *g;
+        VertexFloatMap prm;
+
+    public:
+        Fixture() {
+            boost::minstd_rand gen;
+            const unsigned int nodes = 28250, nearest = 10;
+            const float prob = 0.05;
+            g = new Graph(SWGen(gen, nodes, nearest, prob), SWGen(), nodes);
+            prm = get(&Node::pagerank, graph());
+        }
+
+        virtual ~Fixture() {
+            delete g;
+        }
+
+        bool is_root() {
+            return (process_id(process_group(graph())) == 0);
+        }
+
+        Graph& graph() {
+            return *g;
+        }
+
+        VertexFloatMap& pagerank_map() {
+            return prm;
+        }
+    };
+
+};
+
 namespace Metis
 {
     struct Node {
@@ -103,12 +153,28 @@ namespace Metis
 
 };
 
+typedef adjacency_list <vecS, distributedS<mpi_process_group, vecS>, directedS, SmallWorld::Node> SmallWorldDigraph;
+typedef adjacency_list <vecS, distributedS<mpi_process_group, vecS>, bidirectionalS, SmallWorld::Node> SmallWorldBigraph;
+DECLARE_TYPE_NAME(SmallWorldDigraph);
+DECLARE_TYPE_NAME(SmallWorldBigraph);
+
+typedef mpl::list<SmallWorldDigraph, SmallWorldBigraph> SmallWorldGraphTypes; // PageRank doesn't work with CSR graphs
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(SmallWorldScalabilityTest, G, SmallWorldGraphTypes)
+{
+    benchmark::event_list events;
+    SmallWorld::Fixture<G> f;
+
+    BENCHMARK(events, page_rank(f.graph(), f.pagerank_map()));
+
+    // write_all_results_log("PageRank", GET_TYPE_NAME(f.graph()), events, f.is_root(), false);
+    write_average_log("PageRank", GET_TYPE_NAME(f.graph()), events, f.is_root());
+}
+
 typedef adjacency_list <vecS, distributedS<mpi_process_group, vecS>, directedS, Metis::Node> MetisDigraph;
 typedef adjacency_list <vecS, distributedS<mpi_process_group, vecS>, bidirectionalS, Metis::Node> MetisBigraph;
-typedef compressed_sparse_row_graph<directedS, Metis::Node, no_property, no_property, distributedS<mpi_process_group> > MetisCSRDigraph;
 DECLARE_TYPE_NAME(MetisDigraph);
 DECLARE_TYPE_NAME(MetisBigraph);
-DECLARE_TYPE_NAME(MetisCSRDigraph);
 
 typedef mpl::list<MetisDigraph, MetisBigraph> MetisGraphTypes;
 
@@ -142,10 +208,8 @@ namespace boost
 
 typedef adjacency_list <vecS, distributedS<mpi_process_group, vecS>, directedS, Csv::Node> CsvDigraph;
 typedef adjacency_list <vecS, distributedS<mpi_process_group, vecS>, bidirectionalS, Csv::Node> CsvBigraph;
-typedef compressed_sparse_row_graph<directedS, Csv::Node, no_property, no_property, distributedS<mpi_process_group> > CsvCSRDigraph;
 DECLARE_TYPE_NAME(CsvDigraph);
 DECLARE_TYPE_NAME(CsvBigraph);
-DECLARE_TYPE_NAME(CsvCSRDigraph);
 
 typedef mpl::list<CsvDigraph, CsvBigraph> CsvGraphTypes;
 
