98de49d43c8849b8ffafa346db0f127e4cdd7c9c
Carmine Paolino <earcar@gmail.com>
2011-02-10 19:22:32 +0100

    Forgot marcellina

--- tests/performance/list_csunibo.rb
+++ tests/performance/list_csunibo.rb
@@ -6,7 +6,7 @@ require 'uri'
 URL = 'http://www.cs.unibo.it/servizi/dept/admin/cluster/lab_linux/ssh.html'
 
 def get_servers(url)
-  Net::HTTP.get(URI.parse(url)).scan(/[a-z]+\.cs\.unibo\.it/) - [ 'ssl.cs.unibo.it', 'calaf.cs.unibo.it', 'susanna.cs.unibo.it', 'giovanna.cs.unibo.it', 'edmondo.cs.unibo.it', 'ludovico.cs.unibo.it', 'kurwenal.cs.unibo.it' ]
+  Net::HTTP.get(URI.parse(url)).scan(/[a-z]+\.cs\.unibo\.it/) - [ 'ssl.cs.unibo.it', 'calaf.cs.unibo.it', 'susanna.cs.unibo.it', 'giovanna.cs.unibo.it', 'edmondo.cs.unibo.it', 'ludovico.cs.unibo.it', 'kurwenal.cs.unibo.it', 'marcellina.cs.unibo.it' ]
 end
 
 puts get_servers(URL).join(',')
