c501674138430bd39bad8beadd06796c856f8899
Carmine Paolino <earcar@gmail.com>
2011-02-18 12:08:37 +0100

    Distinguish between Ercolani and Ranzani labs and return machines from the requested lab

--- tests/performance/list_csunibo.rb
+++ tests/performance/list_csunibo.rb
@@ -2,11 +2,42 @@
 # Prints a comma separated list of servers in the cs.unibo.it cluster
 require 'net/http'
 require 'uri'
+require 'rexml/document'
+require 'optparse'
 
 URL = 'http://www.cs.unibo.it/servizi/dept/admin/cluster/lab_linux/ssh.html'
 
-def get_servers(url)
-  Net::HTTP.get(URI.parse(url)).scan(/[a-z]+\.cs\.unibo\.it/) - [ 'ssl.cs.unibo.it', 'calaf.cs.unibo.it', 'susanna.cs.unibo.it', 'giovanna.cs.unibo.it', 'edmondo.cs.unibo.it', 'ludovico.cs.unibo.it', 'kurwenal.cs.unibo.it', 'marcellina.cs.unibo.it' ]
+def get(url)
+  Net::HTTP.get(URI.parse(url))
 end
 
-puts get_servers(URL).join(',')
+def ercolani(document)
+  REXML::XPath.match(document, '//div/table[1]/tbody/tr/td').map { |el| el.text }
+end
+
+def ranzani(document)
+  REXML::XPath.match(document, '//div/table[2]/tbody/tr/td').map { |el| el.text }
+end
+
+options = { :lab => :all }
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]"
+  opts.on('-e', '--ercolani', "Return only Ercolani's workstations") { options[:lab] = :ercolani }
+  opts.on('-r', '--ranzani', "Return only Ranzani's workstations") { options[:lab] = :ranzani }
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end.parse!
+
+doc = REXML::Document.new(get(URL))
+
+case options[:lab]
+when :all
+  puts (ercolani(doc) | ranzani(doc)).join(',')
+when :ercolani
+  puts ercolani(doc).join(',')
+when :ranzani
+  puts ranzani(doc).join(',')
+end
